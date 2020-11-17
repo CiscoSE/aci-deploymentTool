8f5ed26f3207216c9dfacbafc4044e10649297e2
srehling <oldguard@msn.com>
2020-11-17 09:45:15 -0500

    Working Authentication and some basic logging

---
+++ bin/common.py
@@ -0,0 +1,59 @@
+import requests as URL
+# Disable warnings regarding certificates. 
+URL.packages.urllib3.disable_warnings()
+
+class urlFunctions:
+    def __init__(self, args):
+        self.header = {"Content-Type": "application/xml"}
+        self.server = args.server
+        self.sourceFolder = args.sourceFolder
+        self.writeScreen = loggingFunctions().writeScreen
+        self.writeEvent = loggingFunctions().writeEvent
+        return
+
+    def getData(self, url, htmlMethod='GET', data=''):
+        if htmlMethod == "POST":
+            apiResponse = URL.post(url=url, data=data, headers=self.header, verify=False)
+        elif htmlMethod == "GET":
+            apiResponse = URL.get(url=url, headers=self.header, verify=False)
+        return apiResponse
+
+    def getCookie(self, user, password):
+        self.writeScreen(msg='########## Starting Process to Get Cookie ##########')
+        url = "https://{0}{1}".format(self.server, "/api/aaaLogin.xml")
+        self.writeEvent(msg = f"URL: {url}")
+        logonRequest="<aaaUser name='{0}' pwd='{1}' />".format(user,password)
+        getCookieResponse = self.getData(url=url, htmlMethod="POST", data=logonRequest)
+        print(getCookieResponse.status_code)
+        return
+
+class loggingFunctions:
+    def __init__(self):
+        return
+    
+    def writeEvent(self, msg, msgType='INFO'):
+        self.writeScreen(msg, msgType)
+        return
+    
+    def writeLog(self):
+        return
+
+    def writeScreen(self, msg, msgType='INFO'):
+        if msgType == 'INFO':
+            print(f"[ {textColors.INFO}INFO{textColors.noColor} ] {msg}")
+        elif msgType == 'WARN':
+            print(f"[ {textColors.WARN}WARN{textColors.noColor} ] {msg}")
+        elif msgType == "FAIL":
+            print(f"[ {textColors.FAIL}FAIL{textColors.noColor} ] {msg}")
+        else:
+            print(f"[ {textColors.FAIL}UNKNOWN{textColors.noColor} ] {msg}")
+            print(f"This is a developer bug. Script will exit")
+            exit()
+        return
+
+# Colors used in text output that can be called by name. 
+class textColors:
+        noColor = '\x1b[0m'
+        INFO = '\033[32m'
+        WARN = '\033[33m'
+        FAIL = '\033[31m'
---
+++ deployACIChanges.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import argparse
+import getpass
+
+
+
+#Append path for files in the bin directory
+sys.path.append(os.getcwd() + '/bin')
+
+#Import URL functions from the common.py file. We use these for all API communication
+from common import urlFunctions
+
+#clear the terminal window before we continue
+os.system('clear')
+
+#########################
+# Argument Handling
+#########################
+
+helpMsg = """
+Deployment Tool for pushing XML configuration files for Cisco ACI APICs. 
+"""
+
+argsParse = argparse.ArgumentParser(description=helpMsg)
+argsParse.add_argument('-s', '--server',         action='store', dest='server',       default='10.82.66.223', help='APIC IP addresses or FQDN to be changed.' )
+argsParse.add_argument('-u', '--user',           action='store', dest='user',         default='admin',        help='User name for APIC Access' )
+argsParse.add_argument('-p', '--password',       action='store', dest='password',     default='somePassword', help='Password for APIC Access' )
+argsParse.add_argument('-f', '--source-folder',  action='store', dest='sourceFolder', default='somePassword', help='Password for APIC Access' )
+
+args = argsParse.parse_args()
+
+# Functions
+def main():
+    # Initialize API access object
+    URL = urlFunctions(args)
+    #Get Cookie
+    URL.getCookie(args.user, args.password)
+    return
+
+main()
